d9c766b9d7adbe8cd63a637037e8a8d0a28562e1
William Liu <liuwilly4@gmail.com>
2022-01-19 15:02:05 -0500

    wordSearch-pair

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,24 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map((ls) => ls.join(""));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
 
-module.exports = wordSearch
+  if (letters.length) {
+    for (let rows = 0; rows < letters[0].length; rows++) {
+      let tempWord = "";
+      for (let cols = 0; cols < letters.length; cols++) {
+        if (letters[cols][rows]) {
+          tempWord += letters[cols][rows];
+        } else {
+          tempWord += "*";
+        }
+      }
+      if (tempWord.includes(word)) return true;
+    }
+  }
+
+  return false;
+};
+
+module.exports = wordSearch;
